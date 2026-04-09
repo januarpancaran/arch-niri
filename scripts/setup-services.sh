b93b7c9f19bdcf0039015d89ab0a58192ebdcafd
januarpancaran <januar352@gmail.com>
2026-04-09 15:27:09 +0700

    feat: adding setup services script

---
+++ scripts/setup-services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "$script_dir/utils.sh"
+
+SERVICES=(
+    NetworkManager
+    bluetooth
+    gdm
+    warp-svc
+)
+
+enable_services() {
+    enable_service "${SERVICES[@]}"
+}
+
+enable_services
