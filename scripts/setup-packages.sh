9fa24434b979179f9945a4d7228a5c2cfb4926a3
januarpancaran <januar352@gmail.com>
2026-04-09 15:26:56 +0700

    feat: adding initial scripts

---
+++ scripts/setup-packages.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "$script_dir/utils.sh"
+
+PKGS=(
+    acpi
+    bat
+    bluez
+    bluez-utils
+    brightnessctl
+    curl
+    cloudflare-warp-bin
+    fastfetch
+    fzf
+    gdm
+    git
+    gnome-keyring
+    gvfs
+    htop
+    jq
+    less
+    man-db
+    nautilus
+    ntfs-3g
+    openssh
+    os-prober
+    polkit-gnome
+    ripgrep
+    starship
+    tar
+    tmux
+    trash-cli
+    tree
+    unrar
+    unzip
+    wget
+    wl-clipboard
+    wl-mirror
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal-gtk
+    xwayland-satellite
+    niri
+    noctalia-shell
+    xorg-xhost
+    yazi
+    zip
+    zoxide
+    zsh
+)
+
+install_pkgs() {
+    install "${PKGS[@]}"
+}
+
+install_pkgs
---
+++ scripts/setup-pipewire.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "$script_dir/utils.sh"
+
+SOUND_PKGS=(
+    alsa-firmware
+    alsa-utils
+    pipewire
+    pipewire-alsa
+    pipewire-pulse
+    wireplumber
+)
+
+setup_sound() {
+    install "${SOUND_PKGS[@]}"
+
+    enable_user_service pipewire
+}
+
+setup_sound
