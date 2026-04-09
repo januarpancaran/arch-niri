19ca28d0077fb0a7663a1185425e7a64d9ec06a9
januarpancaran <januar352@gmail.com>
2026-04-09 15:30:33 +0700

    feat: adding setup fonts script

---
+++ scripts/setup-fonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "$script_dir/utils.sh"
+
+FONTS=(
+	noto-fonts-cjk
+	noto-fonts-emoji
+	ttf-jetbrains-mono-nerd
+)
+
+install_fonts() {
+    install "${FONTS[@]}"
+}
+
+install_fonts
