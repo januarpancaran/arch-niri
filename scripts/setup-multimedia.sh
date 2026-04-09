bf8ee77590eb8072d4fe291c8d4dd028ed443e01
januarpancaran <januar352@gmail.com>
2026-04-09 19:36:07 +0700

    feat: adding setup multimedia script

---
+++ scripts/setup-multimedia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "$script_dir/utils.sh"
+
+MULTIMEDIA_PKGS=(
+    discord
+    mpv
+    obs-studio
+    spotify
+    telegram-desktop
+)
+
+install_multimedia() {
+    install "${MULTIMEDIA_PKGS[@]}"
+}
+
+install_multimedia
